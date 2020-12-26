9e0881edffa900fbcfd706edcc7b41f7956f7f15
DLzZ2013 <798679633@qq.com>
2020-12-26 23:22:22 +0800

    frame

--- src/main/java/com/dany/blog/controller/LoginController.java
+++ src/main/java/com/dany/blog/controller/LoginController.java
@@ -19,13 +19,11 @@ public class LoginController {
     //说明一组参数
     @ApiImplicitParams({
             @ApiImplicitParam(value = "用户名",name="username",type = "query",dataType = "String",example = "dlzz",required = true),
-            @ApiImplicitParam(value = "密码",name = "password",type = "query",dataType = "String",example = "dlzz",
-                    required = true)
+            @ApiImplicitParam(value = "密码",name = "password",type = "query",dataType = "String",example = "dlzz",required = true)
     })
     @PostMapping("/user")
     @ApiResponses({
-            @ApiResponse(code=702,message = "参数错误"),
-            @ApiResponse(code=703,message = "权限不足")
+            @ApiResponse(code=200,message = "登录成功"),
     })
     public String login(@RequestParam String username,@RequestParam String password)
     {
